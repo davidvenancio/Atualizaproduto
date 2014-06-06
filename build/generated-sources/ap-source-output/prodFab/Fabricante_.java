e58298b4bef70ddc35f64ba33e567ce3bcb9017e
davidvenancio <davidvenancio@me.com>
2014-06-06 00:20:28 -0300

    ProjetoFinal-Unicarioca
    UniCarioca - 853-Projeto Integrador em ADS - Prof. Anderson - Meier II
    - Quinta - 21:40

--- build/generated-sources/ap-source-output/prodFab/Fabricante_.java
+++ build/generated-sources/ap-source-output/prodFab/Fabricante_.java
@@ -6,7 +6,7 @@ import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 import prodFab.Produto;
 
-@Generated(value="EclipseLink-2.5.1.v20130918-rNA", date="2014-06-04T01:26:00")
+@Generated(value="EclipseLink-2.5.1.v20130918-rNA", date="2014-06-05T20:11:56")
 @StaticMetamodel(Fabricante.class)
 public class Fabricante_ { 
 
--- build/generated-sources/ap-source-output/prodFab/Produto_.java
+++ build/generated-sources/ap-source-output/prodFab/Produto_.java
@@ -6,7 +6,7 @@ import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 import prodFab.Fabricante;
 
-@Generated(value="EclipseLink-2.5.1.v20130918-rNA", date="2014-06-04T01:26:00")
+@Generated(value="EclipseLink-2.5.1.v20130918-rNA", date="2014-06-05T20:11:56")
 @StaticMetamodel(Produto.class)
 public class Produto_ { 
 
